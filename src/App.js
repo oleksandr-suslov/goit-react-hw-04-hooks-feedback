486e3a3fa0901488c31b0764fe7763ccd7817ac2
oleksandr-suslov <Oleksandr.Suslov@gmail.com>
2021-08-18 13:15:26 +0300

    task one

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Feedback from "./componets/Feedback/Feedback";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Feedback />
+    </div>
+  );
+}
+
+export default App;
